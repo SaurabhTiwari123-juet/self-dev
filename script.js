76ae696c186da25e6c15cf547542ed9912390eec
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-10-04 12:42:16 +0530

    New3

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.createElement("p");
+// para.textContent = "this is a paragraph using dom api";
+// document.body.appendChild(para);
+
+const para = document.createElement("p");
+para.textContent = "this is a para using dom api";
+document.body.appendChild(para);
